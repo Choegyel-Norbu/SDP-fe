850282265ca4f3462ee6ee119381f05ad2c78d73
Choegyel <mac@Chogyal.local>
2025-04-11 07:10:37 +0600

    app.jsx changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { ToastContainer } from "react-toastify";
 import AppRouting from "./routes/AppRouting";
-import "./app.css";
+// import "./app.css";
 import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 
 function App() {
